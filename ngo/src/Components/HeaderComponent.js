fd8aeca47937fcb8ac01f4c01c05bc151338d77e
thebortnyak <zsombor.bortnyak@gmail.com>
2023-02-13 19:07:25 +0100

    source

---
+++ ngo/src/Components/HeaderComponent.js
@@ -0,0 +1,17 @@
+function Header(props) {
+    
+  
+    let content = 
+      <div className="header">
+        
+        <div className="navbar">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+        </div>
+      </div>;
+  
+    return content;
+  }
+  
+  export default Header;
