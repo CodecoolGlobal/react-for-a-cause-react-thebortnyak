e782bd3af8b434544eefff433693d55235f73fda
thebortnyak <zsombor.bortnyak@gmail.com>
2023-02-15 09:29:28 +0100

    initial commit

---
+++ ngo/src/Components/FormComponent.js
@@ -0,0 +1,13 @@
+function FormComponent(){
+let content = 
+<form>
+<label>
+    Name:
+    <input type="text" name="name"/>
+</label>
+<input type="submit" value="Submit"/>
+</form>
+return content;
+}
+
+export default FormComponent;
